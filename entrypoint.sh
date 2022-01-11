9c4e5885b213cb6b0951bcd277772270f23094ba
Dariusz Porowski <Dariusz.Porowski@microsoft.com>
2022-01-10 21:08:29 -0800

    format

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-function arg(){
+function arg() {
     local _command="${1}"
     local _format="${2}"
     local _val="${3}"
-    
-    if [[ "${#_val}" == 0 || "${_val}" == "false" ]]
-    then
+
+    if [[ "${#_val}" == 0 || "${_val}" == "false" ]]; then
         echo "${_command}"
         return
     fi
@@ -14,23 +13,20 @@ function arg(){
     echo "${_command}${_arg}"
 }
 
-function default(){
+function default() {
     local _default="${1}"
     local _result="${2}"
     local _val="${3}"
     local _defaultifnotset="${4}"
 
-    if [[ "${_defaultifnotset}" == "true" ]]
-    then
-        if [ "${#_val}" = 0 ]
-        then
+    if [[ "${_defaultifnotset}" == "true" ]]; then
+        if [ "${#_val}" = 0 ]; then
             echo "${_default}"
             return
         fi
     fi
 
-    if [[ "${_val}" != "${_default}" ]]
-    then
+    if [[ "${_val}" != "${_default}" ]]; then
         echo "${_result}"
     else
         echo "${_val}"
@@ -59,8 +55,7 @@ echo "INPUT_LOG_LEVEL: ${INPUT_LOG_LEVEL}"
 echo "----------------------------------"
 
 command="gitleaks detect"
-if [ -f "${INPUT_CONFIG}" ]
-then
+if [ -f "${INPUT_CONFIG}" ]; then
     command=$(arg "${command}" '--config %s' "${INPUT_CONFIG}")
 fi
 
@@ -70,8 +65,7 @@ command=$(arg "${command}" '--verbose' "${INPUT_VERBOSE}")
 command=$(arg "${command}" '--log-level %s' "${INPUT_LOG_LEVEL}")
 command=$(arg "${command}" '--report-path %s' "${GITHUB_WORKSPACE}/gitleaks-report.${INPUT_REPORT_FORMAT}")
 
-if [[ "${GITHUB_EVENT_NAME}" == "pull_request" ]]
-then
+if [[ "${GITHUB_EVENT_NAME}" == "pull_request" ]]; then
     command=$(arg "${command}" '--source %s' "${GITHUB_WORKSPACE}")
     command=$(arg "${command}" '--log-opts "%s"' "--all ${GITHUB_HEAD_REF}...${GITHUB_BASE_REF}")
 else
@@ -85,8 +79,7 @@ echo "${command}"
 echo "::set-output name=command::${command}"
 COMMAND_OUTPUT=$(eval "${command}")
 
-if [ $? -eq 1 ]
-then
+if [ $? -eq 1 ]; then
     echo "----------------------------------"
     echo "${COMMAND_OUTPUT}"
     echo "::set-output name=exitcode::1"
@@ -94,8 +87,7 @@ then
     echo "::set-output name=report::gitleaks-report.${INPUT_REPORT_FORMAT}"
     GITLEAKS_RESULT="STOP! Gitleaks encountered leaks or error"
     echo "::set-output name=result::${GITLEAKS_RESULT}"
-    if [ "${INPUT_FAIL}" = "true" ]
-    then
+    if [ "${INPUT_FAIL}" = "true" ]; then
         echo "::error::${GITLEAKS_RESULT}"
         exit 1
     else
